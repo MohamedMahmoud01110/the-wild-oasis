2cdee902111672868038fa75b95bd9636c7f2618
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2026-05-11 21:09:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+// import { useState, useEffect } from "react";
+// import { supabase } from "./utils/supabase";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      // refetchOnWindowFocus: false,
+      // refetchOnReconnect: false,
+      // retry: false,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            {/* <Route index element={<Dashboard />}></Route> */}
+            <Route index element={<Navigate replace to="/dashboard" />}></Route>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/bookings" element={<Bookings />}></Route>
+            <Route path="/cabins" element={<Cabins />}></Route>
+            <Route path="/users" element={<Users />}></Route>
+            <Route path="/settings" element={<Settings />}></Route>
+            <Route path="/account" element={<Account />}></Route>
+          </Route>
+
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
+export default App;
